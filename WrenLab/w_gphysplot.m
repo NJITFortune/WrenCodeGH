5de5204f1e9622a3a466b07077e4935c52b2f3ae
Eric Fortune <eric.fortune@njit.edu>
2018-08-11 23:56:05 -0400

    / ‘WrenLab/w_gphysplot.m’

--- WrenLab/w_gphysplot.m
+++ WrenLab/w_gphysplot.m
@@ -106,10 +106,10 @@ for j=1:3 % For each subplot
         subplot(3,1,j); hold on;
         xx = axis;
     for k=1:length(in.syl)
-        
+        if ~isempty(in.syl(k).tim(1))
         plot([in.syl(k).tim(1), in.syl(k).tim(1)], [xx(3), xx(4)], 'g-');
         plot([in.syl(k).tim(2), in.syl(k).tim(2)], [xx(3), xx(4)], 'r-');        
-                
+        end        
     end
 end
         
