783f0a54bb7643505bb67f044e15cd6273f00d6b
Eric Fortune <eric.fortune@njit.edu>
2018-08-21 13:54:10 -0400

    / ‘WrenLab/wAutoSolo.m’

--- WrenLab/wAutoSolo.m
+++ WrenLab/wAutoSolo.m
@@ -277,13 +277,13 @@ end % curpair (cycle through spons)
     
     out.malPREbintim = malPREtimbin;
     out.femPREbintim = femPREtimbin;
-            figure(5); clf; plot(windowtims, out.femPREbintim, '*-m');
+            figure(15); clf; plot(windowtims, out.femPREbintim, '*-m');
             hold on; plot(windowtims, out.malPREbintim, '*-b');
             
     out.malPOSTbintim = malPOSTtimbin;
     out.femPOSTbintim = femPOSTtimbin;
     
-            figure(6); clf; plot(windowtims, out.femPOSTbintim, '*-m');
+            figure(16); clf; plot(windowtims, out.femPOSTbintim, '*-m');
             hold on; plot(windowtims, out.malPOSTbintim, '*-b');
     
     out.mautospondeg = mautospondeg;
