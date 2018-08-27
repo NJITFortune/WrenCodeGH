496bf3bb31f879308066003c1bbf572f44ca5fc5
Eric Fortune <eric.fortune@njit.edu>
2018-08-27 13:58:12 -0400

    / ‘WrenLab/wData.m’

--- WrenLab/wData.m
+++ WrenLab/wData.m
@@ -91,6 +91,8 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{9} = [];
     fsolosyls{9} = [1 2 3 4]; 
     fduetsyls{9} = [];
-    spon(:,9) = [0, 1.5]; 
-    spon(:,9) = [0, 6.5]; 
+%    spon(:,9) = [0, 1.5]; % Both of the spontaneous ranges are not ideal
+    spon(:,9) = [0, 6.5]; % Both of the spontaneous ranges are not ideal
+    
+    
     
