ca5e7b6190079ca71c8510e59bf0e9f7bff6a4cb
Eric Fortune <eric.fortune@njit.edu>
2018-08-28 17:32:46 -0400

    / ‘WrenLab/wRS_Chronic.m’

--- WrenLab/wRS_Chronic.m
+++ WrenLab/wRS_Chronic.m
@@ -161,7 +161,7 @@ subplot(132); hold on; title('Raw RS');
     errorbar([3 4], meanRaw(3:4), sraw(3:4), 'm' );
         for p=1:length(sumdat.fDuetAuto.rsRaw); plot(3.1, sumdat.fDuetAuto.rsRaw(p), 'k.', 'MarkerSize', 4); end
         for p=1:length(sumdat.fSoloAuto.rsRaw); plot(4.1, sumdat.fSoloAuto.rsRaw(p), 'k.', 'MarkerSize', 4); end
-    ylim([-20 100]); xlim([0.5 4.5]); plot([1,4], [0,0], 'k-');
+    ylim([-10 100]); xlim([0.5 4.5]); plot([1,4], [0,0], 'k-');
 
 subplot(133); hold on; title('Spikes/Second');
     plot([1 2], meanSPS(1:2), 'bo'); 
