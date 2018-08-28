64242874e0dac9f4129f1dde6d074fb89a0d5bb2
Eric Fortune <eric.fortune@njit.edu>
2018-08-28 17:39:30 -0400

    / ‘WrenLab/wRS_Chronic.m’

--- WrenLab/wRS_Chronic.m
+++ WrenLab/wRS_Chronic.m
@@ -145,7 +145,7 @@ subplot(131); hold on; title('Auto Normalized RS');
     errorbar([3 4], meanNorm(3:4), s(3:4), 'm' );
         for p=1:length(sumdat.fDuetAuto.rsNorm); plot(3.1, sumdat.fDuetAuto.rsNorm(p), 'k.', 'MarkerSize', 4); end
         for p=1:length(sumdat.fSoloAuto.rsNorm); plot(4.1, sumdat.fSoloAuto.rsNorm(p), 'k.', 'MarkerSize', 4); end
-    ylim([-5 50]); xlim([0.5 4.5]); plot([1,4], [0,0], 'k-');
+    ylim([-5 40]); xlim([0.5 4.5]); plot([1,4], [0,0], 'k-');
 
 subplot(132); hold on; title('Auto Raw RS');
     plot([1 2], meanRaw(1:2), 'bo'); 
@@ -156,7 +156,7 @@ subplot(132); hold on; title('Auto Raw RS');
     errorbar([3 4], meanRaw(3:4), sraw(3:4), 'm' );
         for p=1:length(sumdat.fDuetAuto.rsRaw); plot(3.1, sumdat.fDuetAuto.rsRaw(p), 'k.', 'MarkerSize', 4); end
         for p=1:length(sumdat.fSoloAuto.rsRaw); plot(4.1, sumdat.fSoloAuto.rsRaw(p), 'k.', 'MarkerSize', 4); end
-    ylim([-10 50]); xlim([0.5 4.5]); plot([1,4], [0,0], 'k-');
+    ylim([-10 100]); xlim([0.5 4.5]); plot([1,4], [0,0], 'k-');
 
 subplot(133); hold on; title('Auto Spikes/Second');
     plot([1 2], meanSPS(1:2), 'bo'); 
@@ -167,7 +167,7 @@ subplot(133); hold on; title('Auto Spikes/Second');
     errorbar([3 4], meanSPS(3:4), sps(3:4), 'm' );
         for p=1:length(sumdat.fDuetAuto.SPS); plot(3.1, sumdat.fDuetAuto.SPS(p), 'k.', 'MarkerSize', 4); end
         for p=1:length(sumdat.fSoloAuto.SPS); plot(4.1, sumdat.fSoloAuto.SPS(p), 'k.', 'MarkerSize', 4); end
-    ylim([0 60]); xlim([0.5 4.5]); 
+    ylim([0 100]); xlim([0.5 4.5]); 
 
     
 %% Plot SENSORY
@@ -213,7 +213,7 @@ subplot(132); hold on; title('Hetero Raw RS');
     errorbar([3 4], meanRaw(3:4), sraw(3:4), 'm' );
         for p=1:length(sumdat.fDuetHetero.rsRaw); plot(3.1, sumdat.fDuetHetero.rsRaw(p), 'k.', 'MarkerSize', 4); end
         for p=1:length(sumdat.fSoloHetero.rsRaw); plot(4.1, sumdat.fSoloHetero.rsRaw(p), 'k.', 'MarkerSize', 4); end
-    ylim([-10 100]); xlim([0.5 4.5]); plot([1,4], [0,0], 'k-');
+    ylim([-10 50]); xlim([0.5 4.5]); plot([1,4], [0,0], 'k-');
 
 subplot(133); hold on; title('Hetero Spikes/Second');
     plot([1 2], meanSPS(1:2), 'bo'); 
@@ -224,7 +224,7 @@ subplot(133); hold on; title('Hetero Spikes/Second');
     errorbar([3 4], meanSPS(3:4), sps(3:4), 'm' );
         for p=1:length(sumdat.fDuetHetero.SPS); plot(3.1, sumdat.fDuetHetero.SPS(p), 'k.', 'MarkerSize', 4); end
         for p=1:length(sumdat.fSoloHetero.SPS); plot(4.1, sumdat.fSoloHetero.SPS(p), 'k.', 'MarkerSize', 4); end
-    ylim([0 100]); xlim([0.5 4.5]); 
+    ylim([0 60]); xlim([0.5 4.5]); 
 
 
     
