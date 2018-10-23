5b641cb58e8f9c8e22daf03d3d925ec87bcd9dac
Laboratory Fortune <eric.fortune@njit.edu>
2018-10-23 17:47:20 -0400

    / ‘BirdLab/bs_raster.m’

--- BirdLab/bs_raster.m
+++ BirdLab/bs_raster.m
@@ -4,16 +4,16 @@ function bs_raster(spiketimes, col)
 % col is the color, e.g. 'b' or 'm'
 % Version 18 March 2015, VERSION 1 November 2008, original JAB 7/25/07
 
-if nargin < 2; col = 'k'; end; % Default to black tick marks
+if nargin < 2; col = 'k'; end % Default to black tick marks
 
 hold on;
 
-for tt = 1:length( spiketimes );
-   for ss = 1:length( spiketimes{tt} );
+for tt = 1:length( spiketimes )
+   for ss = 1:length( spiketimes{tt} )
       plot( ones(1,2).*spiketimes{tt}(ss), [tt-0.3 tt+0.3], col, 'LineWidth', 0.5);
       % plot( ones(1,2).*spiketimes{tt}(ss), [tt-0.3 tt+0.3], 'k', 'LineWidth', 1.5);
-   end;
-end;
+   end
+end
 
 ax = axis;
 axis( [ax(1:2) 0 length( spiketimes )+1] )
