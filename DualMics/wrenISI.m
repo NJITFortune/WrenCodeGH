0f84c0ca63529898a709d6854749443c8a8e1faf
Eric Fortune <eric.fortune@njit.edu>
2018-11-10 18:33:23 -0500

    / ‘DualMics/wrenISI.m’

--- DualMics/wrenISI.m
+++ DualMics/wrenISI.m
@@ -3,14 +3,14 @@ function out = wrenISI(in)
 % Where "in" is a structure for two-microphone recordings of wrens
 % "in" is composed of 
 % day month year        Date of the recording 
+% Location              Name of the study site
+% vision                1 Visual cues present, 0 Visual cues absent
 % distance              Distance between the two cages in meters  
 % femMic                Audio recording from microphone at female cage
 % maleMic               Audio recording from microphone at male cage
 % Fs                    Sample rate of audio recordings
-% fsyl                  
-% Location  
-% msyl      
-% vision    
+% fsyl msyl             Structure with syllable data
+%   fft
 
 % This is not a general tool.
 % out includes the following:
