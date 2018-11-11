b1f8e30f2e48517d08dc59e5af0894c8c87de72b
Eric Fortune <eric.fortune@njit.edu>
2018-11-10 20:35:22 -0500

    / ‘DualMics/wrenExplorer.m’

--- DualMics/wrenExplorer.m
+++ DualMics/wrenExplorer.m
@@ -8,17 +8,17 @@ Fs = duet(idx).Fs;
 prpspc = flipud(gray);
 
 ax(1) = subplot(211); hold on; 
-    specgram(duet(idx).femMic, 1024, Fs, [], 1000); ylim([0 6000]); colormap(prpspc);
+    specgram(duet(idx).femMic, 1024, Fs, [], 1000); ylim([0 6000]); colormap(prpspc); caxis([-10 30]);
     for j=1:length(duet(idx).fsyl) 
-        plot([duet(idx).fsyl(j).syltim(1) duet(idx).fsyl(j).syltim(1)], [500 5500], 'g', 'LineWidth', 2);
-        plot([duet(idx).fsyl(j).syltim(2) duet(idx).fsyl(j).syltim(2)], [500 5500], 'r', 'LineWidth', 2);
+        plot([duet(idx).fsyl(j).syltim(1) duet(idx).fsyl(j).syltim(1)], [500 5500], 'g', 'LineWidth', 1);
+        plot([duet(idx).fsyl(j).syltim(2) duet(idx).fsyl(j).syltim(2)], [500 5500], 'r', 'LineWidth', 1);
     end
 
 ax(2) = subplot(212); hold on;
-    specgram(duet(idx).maleMic, 1024, Fs, [], 1000); ylim([0 6000]); colormap(prpspc);
+    specgram(duet(idx).maleMic, 1024, Fs, [], 1000); ylim([0 6000]); colormap(prpspc); caxis([-10 30]);
     for j=1:length(duet(idx).msyl) 
-        plot([duet(idx).msyl(j).syltim(1) duet(idx).msyl(j).syltim(1)], [500 5500], 'g', 'LineWidth', 2);
-        plot([duet(idx).msyl(j).syltim(2) duet(idx).msyl(j).syltim(2)], [500 5500], 'r', 'LineWidth', 2);
+        plot([duet(idx).msyl(j).syltim(1) duet(idx).msyl(j).syltim(1)], [500 5500], 'g', 'LineWidth', 1);
+        plot([duet(idx).msyl(j).syltim(2) duet(idx).msyl(j).syltim(2)], [500 5500], 'r', 'LineWidth', 1);
     end
 
 %     %% Get lists of syllables
