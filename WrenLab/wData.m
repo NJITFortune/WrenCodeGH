afc2ca1582c7962c9c36e296791bf8cd9c03f344
Eric Fortune <eric.fortune@njit.edu>
2019-02-19 16:56:43 -0500

    / ‘WrenLab/wData.m’

--- WrenLab/wData.m
+++ WrenLab/wData.m
@@ -1,4 +1,4 @@
-function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
+function [msolosyls, mduetsyls, fsolosyls, fduetsyls, Cspon, Aspon] = wData
 % This function only returns the identities of syllables for use in the
 % analysis of the wren chronic data.  Data ChronicCompleat2018d.mat
 
@@ -12,7 +12,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{1} = [5 7 9 11 13];
     fsolosyls{1} = []; 
     fduetsyls{1} = [6 8 10 12 14];
-    spon(:,1) = [-5.5, -0.5]; % This is a mess. 
+    Cspon(:,1) = [-5.5, -0.5]; % This is a mess. 
     Aspon(:,1) = [7, 10.0];
     
 % 3-4: j160806
@@ -22,7 +22,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{2} = [4 6 8 10 12];
     fsolosyls{2} = 1; 
     fduetsyls{2} = [3 5 7 9 11 13];    
-    spon(:,2) = [-5.0, -0.5]; 
+    Cspon(:,2) = [-5.0, -0.5]; 
     Aspon(:,2) = [-5.0, 0.0];
     
 % 5-6: j160807
@@ -31,7 +31,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{3} = [2 4 6 8 10 12 14 16 18];
     fsolosyls{3} = 1; % Solitary
     fduetsyls{3} = [3 4 7 9 11 13 15 17 19];
-    spon(:,3) = [-5.0, 0.0];
+    Cspon(:,3) = [-5.0, 0.0];
     Aspon(:,3) = [-5.0, 0.0];
     
 % 7-8: j160815
@@ -41,7 +41,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     %fsolosyls{4} = [1 2]; % Orig
         fsolosyls{4} = 1; % Solitary
     fduetsyls{4} = [4 6 8];
-    spon(:,4) = [4.5, 7.0];
+    Cspon(:,4) = [4.5, 7.0];
     Aspon(:,4) = [-3.0, 0.0];
 
 % 9-10: j161009
@@ -51,7 +51,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     %fsolosyls{5} = [1 2]; % Orig
         fsolosyls{5} = 1; % Solitary
     fduetsyls{5} = [4 6 8 10 12 14 16 18];
-    spon(:,5) = [-5.0, 0.0];
+    Cspon(:,5) = [-5.0, 0.0];
     Aspon(:,5) = [-5.0, 0.0];
 
 % 11-12: j161022
@@ -61,7 +61,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     %fsolosyls{6} = [1 2 3]; % Orig
         fsolosyls{6} = [1 2]; % Solitary
     fduetsyls{6} = [5 7 9 11 13];
-    spon(:,6) = [7.5, 12.0];
+    Cspon(:,6) = [7.5, 12.0];
     Aspon(:,6) = [-4.0, 0.0];
 
 % 13-14: j17060848
@@ -71,7 +71,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{7} = [7 9 11 13 15 17];
     fsolosyls{7} = []; 
     fduetsyls{7} = [6 8 10 12 14 16];
-    spon(:,7) = [10.0, 15.0];
+    Cspon(:,7) = [10.0, 15.0];
 
 % 15-16: j170081733 
 
@@ -86,7 +86,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{8} = [3 8];
     fsolosyls{8} = 1; 
     fduetsyls{8} = [2 4 5 7 9 10];
-    spon(:,8) = [-5, 0];
+    Cspon(:,8) = [-5, 0];
     
 % 17-18: j160734 
 
@@ -96,7 +96,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{9} = []; % Very low amplitude
     fsolosyls{9} = 1; 
     fduetsyls{9} = [3 4]; % Female sang loudly after male, syllables 3 and 4
-    spon(:,9) = [0, 1.5]; % Both of the spontaneous ranges are not ideal
+    Cspon(:,9) = [0, 1.5]; % Both of the spontaneous ranges are not ideal
 %    spon(:,9) = [0, 6.5]; % Both of the spontaneous ranges are not ideal
 
 
@@ -112,7 +112,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{10} = [];
     fsolosyls{10} = 1; 
     fduetsyls{10} = [];
-    spon(:,10) = [0, 2]; 
+    Cspon(:,10) = [0, 2]; 
 
 % 21-22: j160800b 
 
@@ -122,7 +122,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{11} = [];
     fsolosyls{11} = 1; 
     fduetsyls{11} = [];
-    spon(:,11) = [0, 1.5]; 
+    Cspon(:,11) = [0, 1.5]; 
     
 % 23-24: j160928a 
 
@@ -132,7 +132,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{12} = [];
     fsolosyls{12} = 1; 
     fduetsyls{12} = [];
-    spon(:,12) = [0, 2]; 
+    Cspon(:,12) = [0, 2]; 
     
 % 25-26: j160928b 
 
@@ -142,7 +142,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{13} = [];
     fsolosyls{13} = 1; 
     fduetsyls{13} = [];
-    spon(:,13) = [0, 2]; 
+    Cspon(:,13) = [0, 2]; 
     
 % 27-28: j160934a 
 
@@ -152,7 +152,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{14} = [];
     fsolosyls{14} = 1; 
     fduetsyls{14} = [];
-    spon(:,14) = [0, 2]; 
+    Cspon(:,14) = [0, 2]; 
     
 % 29-30: j160934b 
 
@@ -162,7 +162,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{15} = [];
     fsolosyls{15} = 1; 
     fduetsyls{15} = [];
-    spon(:,15) = [0, 2]; 
+    Cspon(:,15) = [0, 2]; 
     
 % 31-32: j160934c 
 
@@ -172,7 +172,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{16} = [];
     fsolosyls{16} = 1; 
     fduetsyls{16} = [];
-    spon(:,16) = [3, 5]; 
+    Cspon(:,16) = [3, 5]; 
     
 % 33-34: j160934d 
 
@@ -182,7 +182,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{17} = [];
     fsolosyls{17} = 1; 
     fduetsyls{17} = [];
-    spon(:,17) = [0, 2]; 
+    Cspon(:,17) = [0, 2]; 
     
 % 35-36: j160951a 
 
@@ -192,7 +192,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{18} = [];
     fsolosyls{18} = 1; 
     fduetsyls{18} = [];
-    spon(:,18) = [2.5, 4.0]; 
+    Cspon(:,18) = [2.5, 4.0]; 
 
  % 37-38: j160951b 
 
@@ -202,7 +202,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{19} = [];
     fsolosyls{19} = 1; 
     fduetsyls{19} = [];
-    spon(:,19) = [0, 2]; 
+    Cspon(:,19) = [0, 2]; 
    
 % 39-40: j160951c 
 
@@ -212,7 +212,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{20} = [];
     fsolosyls{20} = 1; 
     fduetsyls{20} = [];
-    spon(:,20) = [0, 2]; 
+    Cspon(:,20) = [0, 2]; 
     
 % 41-42: j160951d 
 
@@ -222,7 +222,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     mduetsyls{21} = [];
     fsolosyls{21} = 1; 
     fduetsyls{21} = [];
-    spon(:,21) = [3.5, 5.5]; 
+    Cspon(:,21) = [3.5, 5.5]; 
 
     
     
