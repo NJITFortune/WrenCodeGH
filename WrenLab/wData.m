3a018e7209e072a4ab76947cdd5c39f6d61c2f8b
Eric Fortune <eric.fortune@njit.edu>
2019-02-19 16:48:36 -0500

    / ‘WrenLab/wData.m’

--- WrenLab/wData.m
+++ WrenLab/wData.m
@@ -13,7 +13,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, spon] = wData
     fsolosyls{1} = []; 
     fduetsyls{1} = [6 8 10 12 14];
     spon(:,1) = [-5.5, -0.5]; % This is a mess. 
-    Aspon(:,1) = [-5.0, 0.0];
+    Aspon(:,1) = [7, 10.0];
     
 % 3-4: j160806
 
