922d55ae2194f3930358ed1500ac24f5a41b3fe1
Eric Fortune <eric.fortune@njit.edu>
2019-02-23 14:41:58 -0500

    / ‘WrenLab/wPhasePlot.m’

--- WrenLab/wPhasePlot.m
+++ WrenLab/wPhasePlot.m
@@ -9,32 +9,34 @@ if nargin < 2; pad = 0; end
 
 [msolosyls, mduetsyls, fsolosyls, fduetsyls, Cspon, Aspon] = wData;
 
+% User can choose which data to analyze
 % birdlist = [3 4 9 10 11 12 13 14];
+birdlist = 1:12; % All compleat data
 
-birdlist = 1:12;
 
 %% For each bird
 for ff = birdlist
-    
-    
-% List of all duet syllables
-allduetsyls = sort([mduetsyls{ceil(ff/2)}, fduetsyls{ceil(ff/2)}]);
+        
+% List of all duet syllables for this bird
+    allduetsyls = sort([mduetsyls{ceil(ff/2)}, fduetsyls{ceil(ff/2)}]);
 
 %% Get all male-female and female-male duet syllable transitions
-goodmalesyls = []; goodfemsyls = [];
+    goodmalesyls = []; goodfemsyls = [];
 
-for t = 1:length(mduetsyls{ceil(ff/2)})
-   if ~isempty(find(allduetsyls == mduetsyls{ceil(ff/2)}(t)+1, 1))
-       goodmalesyls(end+1) = mduetsyls{ceil(ff/2)}(t);
-   end
-end
-for t = 1:length(fduetsyls{ceil(ff/2)})
-   if ~isempty(find(allduetsyls == fduetsyls{ceil(ff/2)}(t)+1, 1))
-       goodfemsyls(end+1) = fduetsyls{ceil(ff/2)}(t);
-   end
-end
+    % Male to female transitions
+    for t = 1:length(mduetsyls{ceil(ff/2)}) 
+       if ~isempty(find(allduetsyls == mduetsyls{ceil(ff/2)}(t)+1, 1))
+           goodmalesyls(end+1) = mduetsyls{ceil(ff/2)}(t);
+       end
+    end
+    % Female to male transitions
+    for t = 1:length(fduetsyls{ceil(ff/2)})
+       if ~isempty(find(allduetsyls == fduetsyls{ceil(ff/2)}(t)+1, 1))
+           goodfemsyls(end+1) = fduetsyls{ceil(ff/2)}(t);
+       end
+    end
 
-%% Get all of the syllable times
+%% Get all of the syllable times (previous start, middle of interval, next end)
 for zz = length(goodmalesyls):-1:1   
     Msyltims(zz,1) = in(ff).syl(goodmalesyls(zz)).tim(1)-pad;
     Msyltims(zz,2) = mean([in(ff).syl(goodmalesyls(zz)+1).tim(1), in(ff).syl(goodmalesyls(zz)).tim(2) ]);
@@ -56,12 +58,6 @@ if in(ff).sexy == 1 % This is a male
     [out(ff).AH.Cvs, out(ff).AH.cPhaseSpikes, out(ff).AH.cSpikes] = wPhasor(in(ff).Cspikes, Msyltims);
     [out(ff).HA.Uvs, out(ff).HA.uPhaseSpikes, out(ff).HA.uSpikes] = wPhasor(in(ff).Aspikes, Fsyltims);
     [out(ff).HA.Cvs, out(ff).HA.cPhaseSpikes, out(ff).HA.cSpikes] = wPhasor(in(ff).Cspikes, Fsyltims);   
-    
-figure(ff); clf;
-subplot(411); bs_swPSTH(out(ff).AH.cPhaseSpikes,[0, plotwid], plotwind, 1); 
-subplot(412); bs_swPSTH(out(ff).AH.uPhaseSpikes,[0, plotwid], plotwind, 6);
-subplot(413); bs_swPSTH(out(ff).HA.cPhaseSpikes,[0, plotwid], plotwind, 1);
-subplot(414); bs_swPSTH(out(ff).HA.uPhaseSpikes,[0, plotwid], plotwind, 6);
 end
 
 if in(ff).sexy == 2 % This is a female
@@ -69,22 +65,7 @@ if in(ff).sexy == 2 % This is a female
     [out(ff).AH.Cvs, out(ff).AH.cPhaseSpikes, out(ff).AH.cSpikes] = wPhasor(in(ff).Cspikes, Fsyltims);
     [out(ff).HA.Uvs, out(ff).HA.uPhaseSpikes, out(ff).HA.uSpikes] = wPhasor(in(ff).Aspikes, Msyltims);
     [out(ff).HA.Cvs, out(ff).HA.cPhaseSpikes, out(ff).HA.cSpikes] = wPhasor(in(ff).Cspikes, Msyltims);    
-
-figure(ff); clf;
-subplot(411); bs_swPSTH(out(ff).AH.cPhaseSpikes,[0, plotwid], plotwind, 2); 
-subplot(412); bs_swPSTH(out(ff).AH.uPhaseSpikes,[0, plotwid], plotwind, 6);
-subplot(413); bs_swPSTH(out(ff).HA.cPhaseSpikes,[0, plotwid], plotwind, 2);
-subplot(414); bs_swPSTH(out(ff).HA.uPhaseSpikes,[0, plotwid], plotwind, 6);
 end
-
-
-
-%     subplot(411); bs_raster(out(ff).AH.cp); 
-%     subplot(412); bs_raster(out(ff).AH.up); 
-%     subplot(413); bs_raster(out(ff).HA.cp); 
-%     subplot(414); bs_raster(out(ff).HA.up); 
-
-
 
 end % Cycle for every bird
 
@@ -95,11 +76,28 @@ for ff = birdlist
     out(ff).maxUV = max([out(ff).HA.Uvs out(ff).AH.Uvs]);     
 end
 
+[stts.H, stts.P, stts.CI] = ttest2([out(1:2:end).maxUV], [out(2:2:end).maxUV]);
 
-[stts.H, stts.P, stts.CI] = ttest2([out(1:2:end).maxUV], [out(2:2:end).maxUV]) 
+%% Use this to plot each bird
+for ff = birdlist
+    if in(ff).sexy == 1 % This is a male
+        figure(ff); clf;
+        subplot(411); bs_swPSTH(out(ff).AH.cPhaseSpikes,[0, plotwid], plotwind, 1); 
+        subplot(412); bs_swPSTH(out(ff).AH.uPhaseSpikes,[0, plotwid], plotwind, 6);
+        subplot(413); bs_swPSTH(out(ff).HA.cPhaseSpikes,[0, plotwid], plotwind, 1);
+        subplot(414); bs_swPSTH(out(ff).HA.uPhaseSpikes,[0, plotwid], plotwind, 6);
+    end
 
+    if in(ff).sexy == 2 % This is a female
+    figure(ff); clf;
+        subplot(411); bs_swPSTH(out(ff).AH.cPhaseSpikes,[0, plotwid], plotwind, 2); 
+        subplot(412); bs_swPSTH(out(ff).AH.uPhaseSpikes,[0, plotwid], plotwind, 6);
+        subplot(413); bs_swPSTH(out(ff).HA.cPhaseSpikes,[0, plotwid], plotwind, 2);
+        subplot(414); bs_swPSTH(out(ff).HA.uPhaseSpikes,[0, plotwid], plotwind, 6);
+end
+end
 
-
+%% Embedded vector strength function
 function [vector_strength, phasespikes, regularspikes] = wPhasor(spiketimes, tims)
 
     wid = 0.250;
