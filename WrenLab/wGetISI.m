8cb34174da3d25fe0df8ead8e35fb9997e06d4a7
Eric Fortune <eric.fortune@gmail.com>
2019-03-06 19:52:16 -0500

    + ‘WrenLab/wGetISI.m’

---
+++ WrenLab/wGetISI.m
@@ -0,0 +1,26 @@
+function out = wGetISI(in)
+% Usage out = wGetISI(in)
+
+% Get the list of duet syllables
+[~, mduetsyls, ~, fduetsyls, ~, ~] = wData;
+
+out.m = []; out.f = [];
+
+
+for j=1:length(mduetsyls)    
+    for k=1:length(mduetsyls{j})        
+        if ~isempty(find(fduetsyls{j} == mduetsyls{j}(k)+1, 1))
+            out.m(end+1) = in(j*2).syl(mduetsyls{j}(k)+1).tim(1) - in(j*2).syl(mduetsyls{j}(k)).tim(2);
+        end
+    end
+end
+
+for j=1:length(fduetsyls)    
+    for k=1:length(fduetsyls{j})        
+        if ~isempty(find(mduetsyls{j} == fduetsyls{j}(k)+1, 1))
+            out.f(end+1) = in(j*2).syl(fduetsyls{j}(k)+1).tim(1) - in(j*2).syl(fduetsyls{j}(k)).tim(2);
+        end
+    end
+end
+
+
