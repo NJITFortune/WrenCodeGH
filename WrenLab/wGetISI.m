aa50772a0f4ec0dafd0b9129c830c33ff885c0ee
Eric Fortune <eric.fortune@njit.edu>
2019-04-17 19:54:08 -0400

    / ‘WrenLab/wGetISI.m’

--- WrenLab/wGetISI.m
+++ WrenLab/wGetISI.m
@@ -4,13 +4,13 @@ function out = wGetISI(in)
 % Get the list of duet syllables
 [~, mduetsyls, ~, fduetsyls, ~, ~] = wData;
 
-out.HAm = []; out.HAf = [];
+out.FM = []; out.MF = [];
 
 
 for j=1:length(mduetsyls)    % For each duet
     for k=1:length(mduetsyls{j}) % For each male duet syllable in each duet
         if ~isempty(find(fduetsyls{j} == mduetsyls{j}(k)-1, 1)) % If there was a prior female syllable
-            out.HAm(end+1) = in(j*2).syl(mduetsyls{j}(k)).tim(1) - in(j*2).syl(mduetsyls{j}(k)-1).tim(2);
+            out.FM(end+1) = in(j*2).syl(mduetsyls{j}(k)).tim(1) - in(j*2).syl(mduetsyls{j}(k)-1).tim(2);
         end
     end
 end
@@ -18,7 +18,7 @@ end
 for j=1:length(fduetsyls)    
     for k=1:length(fduetsyls{j})        
         if ~isempty(find(mduetsyls{j} == fduetsyls{j}(k)-1, 1))
-            out.HAf(end+1) = in(j*2).syl(fduetsyls{j}(k)).tim(1) - in(j*2).syl(fduetsyls{j}(k)-1).tim(2);
+            out.MF(end+1) = in(j*2).syl(fduetsyls{j}(k)).tim(1) - in(j*2).syl(fduetsyls{j}(k)-1).tim(2);
         end
     end
 end
