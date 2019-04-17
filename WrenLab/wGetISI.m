cd3ee09c774a4d240ec5c1ad96f2705945b5b6ac
Eric Fortune <eric.fortune@njit.edu>
2019-04-17 19:47:45 -0400

    / ‘WrenLab/wGetISI.m’

--- WrenLab/wGetISI.m
+++ WrenLab/wGetISI.m
@@ -10,6 +10,7 @@ out.HAm = []; out.f = [];
 for j=1:length(mduetsyls)    % For each duet
     for k=1:length(mduetsyls{j}) % For each male duet syllable in each duet
         if ~isempty(find(fduetsyls{j} == mduetsyls{j}(k)-1, 1)) % If there was a prior female syllable
+            mduetsyls{j}(k)-1
             out.HAm(end+1) = in(j*2).syl(mduetsyls{j}(k)).tim(1) - in(j*2).syl(mduetsyls{j}(k)-1).tim(2);
         end
     end
