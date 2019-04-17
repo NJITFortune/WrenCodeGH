41a239a7e21238b2e5fcaf7ba6276940e7f1809f
Eric Fortune <eric.fortune@njit.edu>
2019-04-17 19:40:40 -0400

    / ‘WrenLab/wGetISI.m’

--- WrenLab/wGetISI.m
+++ WrenLab/wGetISI.m
@@ -9,16 +9,16 @@ out.m = []; out.f = [];
 
 for j=1:length(mduetsyls)    
     for k=1:length(mduetsyls{j})
-        if ~isempty(find(fduetsyls{j} == mduetsyls{j}(k)+1, 1))
-            out.m(end+1) = in(j*2).syl(mduetsyls{j}(k)+1).tim(1) - in(j*2).syl(mduetsyls{j}(k)).tim(2);
+        if ~isempty(find(fduetsyls{j} == mduetsyls{j}(k)-1, 1))
+            out.m(end+1) = in(j*2).syl(mduetsyls{j}(k)).tim(1) - in(j*2).syl(mduetsyls{j}(k)-1).tim(2);
         end
     end
 end
 
 for j=1:length(fduetsyls)    
     for k=1:length(fduetsyls{j})        
-        if ~isempty(find(mduetsyls{j} == fduetsyls{j}(k)+1, 1))
-            out.f(end+1) = in(j*2).syl(fduetsyls{j}(k)+1).tim(1) - in(j*2).syl(fduetsyls{j}(k)).tim(2);
+        if ~isempty(find(mduetsyls{j} == fduetsyls{j}(k)-1, 1))
+            out.f(end+1) = in(j*2).syl(fduetsyls{j}(k)).tim(1) - in(j*2).syl(fduetsyls{j}(k)-1).tim(2);
         end
     end
 end
