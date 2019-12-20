dbe4029c576915cdf5637eac645bc2af0fab1ea0
Eric Fortune <eric.fortune@gmail.com>
2019-12-20 17:45:58 -0500

    / ‘WrenLab/wTransitions.m’

--- WrenLab/wTransitions.m
+++ WrenLab/wTransitions.m
@@ -50,14 +50,16 @@ sylstrdx = ceil(ff/2); % Apologies. The syllable indices from wData.m
     for z = 1:length(in(ff).Cspikes)
         ChronSpon(ff) = ChronSpon(ff) + length(in(ff).Cspikes{z} > Cspon(1,sylstrdx) & in(ff).Cspikes{z} < Cspon(2,sylstrdx));
     end
-        ChronSpon(ff) = ChronSpon(ff)/length(in(ff).Cspikes); 
+        ChronSpon(ff) = ChronSpon(ff) / length(in(ff).Cspikes); % Divide by number of reps
+        ChronSpon(ff) = ChronSpon(ff) / (Cspon(2,sylstrdx) - Cspon(1,sylstrdx)); % Divide by duration 
         
     AcuteSpon(ff) = 0;
     if sylstrdx < 7
         for z = 1:length(in(ff).Aspikes)
             AcuteSpon(ff) = AcuteSpon(ff) + length(in(ff).Aspikes{z} > Aspon(1,sylstrdx) & in(ff).Aspikes{z} < Aspon(2,sylstrdx));
         end
-            AcuteSpon(ff) = AcuteSpon(ff)/length(in(ff).Aspikes); 
+            AcuteSpon(ff) = AcuteSpon(ff) / length(in(ff).Aspikes); 
+            AcuteSpon(ff) = AcuteSpon(ff) / (Aspon(2,sylstrdx) - Aspon(1,sylstrdx)); 
     end
 
 % Get all male-female and female-male duet syllable transitions
