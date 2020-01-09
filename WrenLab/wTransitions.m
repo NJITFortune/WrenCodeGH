f7331d4817a11062f3eaeb6025f198c61f6e8c04
Eric Fortune <eric.fortune@njit.edu>
2020-01-09 01:52:58 -0500

    / ‘WrenLab/wTransitions.m’

--- WrenLab/wTransitions.m
+++ WrenLab/wTransitions.m
@@ -7,7 +7,7 @@ function [M, F] = wTransitions(in)
 
 %% Preparations
 % Default window width for histogram if user didn't specify window
-    widow = 0.400; % 300 msec looks pretty good with numbins 10 and overlap 50
+    widow = 0.300; % 300 msec looks pretty good with numbins 10 and overlap 50
     numbins = 5; % How many bins before and after the onset of our focal syllable?
     
     windur = widow / numbins;
