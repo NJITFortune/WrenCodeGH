93af4b9f0c698965c9d6fcb17b4d865ae3259e48
Eric Fortune <eric.fortune@njit.edu>
2020-01-09 01:25:05 -0500

    / ‘WrenLab/wTransitions.m’

--- WrenLab/wTransitions.m
+++ WrenLab/wTransitions.m
@@ -383,9 +383,9 @@ function tuo = concatHist(xin)
         
     for qq = length(xin):-1:1
             for rr = 1:length(xin(qq).SPS)
+                length(xin(qq).RSraw)
                 if ~exist('cSPS', 'var')
                     cSPS(:,1) = xin(qq).SPS(rr);
-xin(qq).RSraw(rr)
                     cRSrw(:,1) = xin(qq).RSraw(rr);
                     cRSnrm(:,1) = xin(qq).RSnorm(rr);
                 else
