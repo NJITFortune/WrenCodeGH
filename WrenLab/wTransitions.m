ba0efe166b8ec9bf15e9254ba94ca4c348170bef
Eric Fortune <eric.fortune@njit.edu>
2020-01-08 21:16:13 -0500

    / ‘WrenLab/wTransitions.m’

--- WrenLab/wTransitions.m
+++ WrenLab/wTransitions.m
@@ -196,11 +196,11 @@ figure(1); clf; set(gcf, 'Color', [1,1,1]);
 % PLOT M2F DATA 
 ax(1) = subplot(221); hold on; title('M2F Chronic'); plot([0 0], [0 1], 'k-', 'LineWidth', 2);
 % Male
-fill([bins4plot bins4plot(end:-1:1)], [msMAHC.mean - msMAHC.std/2, msMAHC.mean(end:-1:1) + msMAHC.std(end:-1:1)/2], [0.6, 0.9, 0.9], 'LineStyle', 'none');
-plot(bins4plot, msMAHC.mean, 'b-', 'LineWidth', 2, 'Marker', '.', 'MarkerSize', 10);
+fill([bins4plot bins4plot(end:-1:1)], [msMAHC.meanRSraw - msMAHC.stdRSraw/2, msMAHC.meanRSraw(end:-1:1) + msMAHC.stdRSraw(end:-1:1)/2], [0.6, 0.9, 0.9], 'LineStyle', 'none');
+plot(bins4plot, msMAHC.meanRSraw, 'b-', 'LineWidth', 2, 'Marker', '.', 'MarkerSize', 10);
 % Female
-fill([bins4plot bins4plot(end:-1:1)], [msFHAC.mean - msFHAC.std/2, msFHAC.mean(end:-1:1) + msFHAC.std(end:-1:1)/2], [0.9, 0.7, 0.9], 'LineStyle', 'none');
-plot(bins4plot, msFHAC.mean, 'm-', 'LineWidth', 2, 'Marker', '.', 'MarkerSize', 10);
+fill([bins4plot bins4plot(end:-1:1)], [msFHAC.meanRSraw - msFHAC.stdRSraw/2, msFHAC.meanRSraw(end:-1:1) + msFHAC.stdRSraw(end:-1:1)/2], [0.9, 0.7, 0.9], 'LineStyle', 'none');
+plot(bins4plot, msFHAC.meanRSraw, 'm-', 'LineWidth', 2, 'Marker', '.', 'MarkerSize', 10);
 
 ax(2) = subplot(223); hold on; title('M2F Urethane'); plot([0 0], [0 1], 'k-', 'LineWidth', 2);
 set(ax(2),'Color', [0.9, 0.9, 0.9]);
