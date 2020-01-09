f4dd2eda5b7260846d1affa356db7296ae108c8f
Eric Fortune <eric.fortune@njit.edu>
2020-01-09 01:27:22 -0500

    / ‘WrenLab/wTransitions.m’

--- WrenLab/wTransitions.m
+++ WrenLab/wTransitions.m
@@ -470,7 +470,8 @@ function [out, bintims] = wPhaseHist(spiketimes, tims, wid, numbin, sponSPS)
 
         for j=1:length(goodlist)
             out(j).SPS = spikearray(:, goodlist(j)) / binwid;
-            out(j).RSraw = out(j).SPS - sponSPS
+out(j).SPS - sponSPS
+            out(j).RSraw = out(j).SPS - sponSPS;
             out(j).RSnorm = out(j).RSraw / sponSPS;
         end
         
