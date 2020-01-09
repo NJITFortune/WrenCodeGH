34defbbbcc56ef36df2daff281857382e2c1308f
Eric Fortune <eric.fortune@njit.edu>
2020-01-09 16:08:37 -0500

    / ‘WrenLab/wData.m’

--- WrenLab/wData.m
+++ WrenLab/wData.m
@@ -54,7 +54,7 @@ function [msolosyls, mduetsyls, fsolosyls, fduetsyls, Cspon, Aspon] = wData
     %    fsolosyls{5} = 1; % Solitary
     fduetsyls{5} = [4 6 8 10 12 14 16 18];
     Cspon(:,5) = [-4.0, 0.0];
-    Aspon(:,5) = [-5.0, 0.0];
+    Aspon(:,5) = [-4.0, 0.0];
 
 % 11-12: j161022
     
