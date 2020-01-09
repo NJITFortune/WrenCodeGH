5c05fe65f1c9db45c12806d4a9c0f36ecd30d49a
Eric Fortune <eric.fortune@njit.edu>
2020-01-08 23:57:08 -0500

    / ‘WrenLab/wTransitions.m’

--- WrenLab/wTransitions.m
+++ WrenLab/wTransitions.m
@@ -354,12 +354,12 @@ function tuo = concatHist(xin)
     end
         
     for j = length(xin(1).SPS):-1:1
-        meanSPS(j) = nanmean(SPS(j,:)); stdSPS(j) = nanstd(SPS(j,:)); 
-            steSPS(j) = stdSPS(j) / sqrt(length(~isnan(SPS(j,:))));
-        meanRSnorm(j) = nanmean(RSnrm(j,:)); stdRSnorm(j) = nanstd(RSnrm(j,:));
-            steRSnorm(j) = stdRSnorm(j) / sqrt(length(~isnan(RSnrm(j,:))));
-        meanRSraw(j) = nanmean(SPS(j,:)); stdRSraw(j) = nanstd(RSrw(j,:));
-            steRSraw(j) = stdRSraw(j) / sqrt(length(~isnan(RSrw(j,:))));
+        meanSPS(j) = mean(SPS(j,:)); stdSPS(j) = std(SPS(j,:)); 
+            steSPS(j) = stdSPS(j) / sqrt(length(xin));
+        meanRSnorm(j) = mean(RSnrm(j,:)); stdRSnorm(j) = std(RSnrm(j,:));
+            steRSnorm(j) = stdRSnorm(j) / sqrt(length(xin));
+        meanRSraw(j) = mean(SPS(j,:)); stdRSraw(j) = std(RSrw(j,:));
+            steRSraw(j) = stdRSraw(j) / sqrt(length(xin));
     end
         
     tuo.meanSPS = meanSPS;
