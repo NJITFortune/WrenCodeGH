5977fb01407fdcdb273f76ece4e2a7f6194fcb43
Eric Fortune <eric.fortune@njit.edu>
2020-01-09 10:54:12 -0500

    / ‘WrenLab/wTransitions.m’

--- WrenLab/wTransitions.m
+++ WrenLab/wTransitions.m
@@ -111,7 +111,7 @@ for ff = birdlist
     end
 
 %% Generate the transition histograms
-currF2Msyltim
+currMsolosyltims
 if in(ff).sexy == 1 % This is a male
     
     % Use the PhaseCut embedded function to create the histogram for Duet
