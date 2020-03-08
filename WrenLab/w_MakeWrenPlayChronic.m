44b993a74b7f00d7dad35cd5192e3fc67f7b6e47
Eric Fortune <eric.fortune@gmail.com>
2020-03-08 12:52:15 -0400

    + ‘WrenLab/w_MakeWrenPlayChronic.m’

---
+++ WrenLab/w_MakeWrenPlayChronic.m
@@ -0,0 +1,49 @@
+
+Fs = 10000;
+
+spiketim = 1/Fs:1/Fs:0.005; % 5 msec duration for our fake spikes
+
+fspike = sin(2*pi*4000*spiketim) * 0.2 ; % 4 kHz for females
+mspike = sin(2*pi*3000*spiketim) * 0.2 ; % 3 kHz for males
+
+idx = 1; % This is the Male
+
+fem = zeros(1,length(w(idx).tim));
+mal = zeros(1,length(w(idx).tim));
+
+len = length(mspike);
+
+for k = 1:4
+for j = 1:length(w(idx).Cspikes{k})
+   
+    curidx = find(w(idx).tim >= w(idx).Cspikes{k}(j), 1, 'first');
+    
+    if curidx+len-1 < length(w(idx).tim)
+         
+        mal(curidx:curidx+len-1) = mspike;
+        
+    end
+    
+end
+end
+
+idx = 2;
+
+for k = 1:4
+for j = 1:length(w(idx).Cspikes{k})
+   
+    curidx = find(w(idx).tim >= w(idx).Cspikes{k}(j), 1, 'first');
+    
+    if curidx+len-1 < length(w(idx).tim)
+        
+        fem(curidx:curidx+len-1) = fspike;
+        
+    end
+    
+end
+end
+
+spks = fem' + mal';
+
+combo = spks + (w(idx).duet*0.8);
+audiowrite('combo.wav', combo, 10000);
