0346c1683753b9fb15bf6482134c2f4b7633503b
Eric Fortune <eric.fortune@njit.edu>
2020-03-09 07:57:54 -0400

    / ‘WrenLab/w_MakeWrenPlayChronic.m’

--- WrenLab/w_MakeWrenPlayChronic.m
+++ WrenLab/w_MakeWrenPlayChronic.m
@@ -50,5 +50,30 @@ audiowrite('combo.wav', combo, 10000);
 %% Make the video
 
 figure(1); clf; hold on;
+    truesize([640 480]);
     specgram(outduet, 512, Fs);
     colormap('HOT');
+
+    
+% Initialize the "object" that will be the final movie
+writerObj = VideoWriter('mymovie.avi');
+writerObj.FrameRate = 20;
+%writerObj.VideoFormat = 'RGB24';
+writerObj.Quality = 90;
+open(writerObj);
+
+
+
+for i = 100:1000
+    figure(2); clf;
+    imshow(im(i).gray); 
+    hold on; 
+    plot(data.x(i), data.y(i), 'm*', data.x(i-90:i), data.y(i-90:i), 'm-'); 
+    hold off;
+    frame = getframe(gcf);
+    writeVideo(writerObj, frame);
+
+end;
+
+
+close(writerObj);
