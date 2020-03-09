f5b05c26583cb28d02de0b49475a37bd26631e22
Eric Fortune <eric.fortune@gmail.com>
2020-03-09 09:37:53 -0400

    / ‘WrenLab/w_MakeWrenPlayChronic.m’

--- WrenLab/w_MakeWrenPlayChronic.m
+++ WrenLab/w_MakeWrenPlayChronic.m
@@ -69,15 +69,13 @@ figure(1); clf;
     writerObj.Quality = 90;
     open(writerObj);
 
-for i = 100:1000
-    figure(2); clf;
-    imshow(im(i).gray); 
+for i = outim(1):outim/30:outtim(end)
+    plot(
     hold on; 
     plot(data.x(i), data.y(i), 'm*', data.x(i-90:i), data.y(i-90:i), 'm-'); 
     hold off;
-    frame = getframe(gcf);
-    writeVideo(writerObj, frame);
-
+%    frame = getframe(gcf);
+%    writeVideo(writerObj, frame);
 end
 
 
