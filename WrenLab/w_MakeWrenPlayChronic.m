1fd68682a201ae4ad98e21f2c189879f1df0a9a9
Eric Fortune <eric.fortune@gmail.com>
2020-03-09 10:43:46 -0400

    / ‘WrenLab/w_MakeWrenPlayChronic.m’

--- WrenLab/w_MakeWrenPlayChronic.m
+++ WrenLab/w_MakeWrenPlayChronic.m
@@ -1,6 +1,6 @@
 %% Make the audio file
 idx = [1, 2] ; % This is the Male (odd)
-rango = [0.2, 6.2];
+rango = [0.1, 5.4];
 
     specpos = 0;
     if rango(1) < 0; specpos = abs(rango(1)); end
@@ -104,7 +104,7 @@ for vtim = outim(1):1:outim(end)
            femalespkidx = find(w(idx(2)).Cspikes{j} > rango(1) & w(idx(2)).Cspikes{j} < vtim);
            
            for k = 1:length(malespkidx)
-               plot([specpos+w(idx(1)).Cspikes{j}(malespkidx(k)), specpos+w(idx(1)).Cspikes{j}(malespkidx(k))], [200+(j*100), 200+(j*100)+90], 'c-', 'LineWidth', 1);
+               plot([specpos+w(idx(1)).Cspikes{j}(malespkidx(k)), specpos+w(idx(1)).Cspikes{j}(malespkidx(k))], [200+(j*100), 200+(j*100)+90], 'b-', 'LineWidth', 1);
            end
            for k = 1:length(femalespkidx)
                plot([specpos+w(idx(2)).Cspikes{j}(femalespkidx(k)), specpos+w(idx(2)).Cspikes{j}(femalespkidx(k))], [4000+(j*100), 4000+(j*100)+90], 'm-', 'LineWidth', 1);
