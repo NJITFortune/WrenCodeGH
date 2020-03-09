19bc3dc65f2ce226209476c27ea83d0de7954703
Eric Fortune <eric.fortune@njit.edu>
2020-03-08 21:38:40 -0400

    / ‘WrenLab/w_MakeWrenPlayChronic.m’

--- WrenLab/w_MakeWrenPlayChronic.m
+++ WrenLab/w_MakeWrenPlayChronic.m
@@ -1,6 +1,6 @@
 
 idx = 1; % This is the Male (odd)
-
+rango = [-2, 7];
 Fs = w(idx).Fs;
 
 % Make the Fake Spikes
@@ -11,14 +11,18 @@ Fs = w(idx).Fs;
     fspike = sin(2*pi*4000*spiketim) * 0.2 ; % 4 kHz for females
     mspike = sin(2*pi*3000*spiketim) * 0.2 ; % 3 kHz for males
     
+    outim = w(idx).tim(w(idx).tim > rango(1) & w(idx).tim < rango(2));
+    outduet = w(idx).duet(w(idx).tim > rango(1) & w(idx).tim < rango(2));
+    
 % Make the spike sound trains
-    fem = zeros(1,length(w(idx).tim));
-    mal = zeros(1,length(w(idx).tim));
+    fem = zeros(1,length(outim));
+    mal = zeros(1,length(outim));
 
 for k = 1:4
-    for j = 1:length(w(idx).Cspikes{k})   
-        curidx = find(w(idx).tim >= w(idx).Cspikes{k}(j), 1, 'first');    
-    if curidx+len-1 < length(w(idx).tim)  % Need the if not to go over the end.   
+    spkidx = find(w(idx).Cspikes{k} > rango(1) & w(idx).Cspikes{k} < rango(2));
+    for j = 1:length(spkidx)   
+        curidx = find(outim >= w(idx).Cspikes{k}(spkidx(j)), 1, 'first');    
+    if curidx+len-1 < length(outim)  % Need the if not to go over the end.   
         mal(curidx:curidx+len-1) = mspike;
     end
     
@@ -28,11 +32,12 @@ end
 idx = 2;
 
 for k = 1:4
-    for j = 1:length(w(idx).Cspikes{k})
-    curidx = find(w(idx).tim >= w(idx).Cspikes{k}(j), 1, 'first');   
-    if curidx+len-1 < length(w(idx).tim)        
-        fem(curidx:curidx+len-1) = fspike;        
-    end    
+    spkidx = find(w(idx).Cspikes{k} > rango(1) & w(idx).Cspikes{k} < rango(2));
+    for j = 1:length(spkidx)   
+        curidx = find(outim >= w(idx).Cspikes{k}(spkidx(j)), 1, 'first');    
+    if curidx+len-1 < length(outim)  % Need the if not to go over the end.   
+        fem(curidx:curidx+len-1) = mspike;
+    end
     
     end
 end
