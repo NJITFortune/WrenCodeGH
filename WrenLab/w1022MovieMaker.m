f36c896f3d2f1d7b680bdc93d56bfe2ef5718786
Eric Fortune <eric.fortune@njit.edu>
2020-03-15 13:47:48 -0400

    / ‘WrenLab/w1022MovieMaker.m’

--- WrenLab/w1022MovieMaker.m
+++ WrenLab/w1022MovieMaker.m
@@ -44,7 +44,7 @@ hAx = axes('Parent',hFig,'Units','pixels','NextPlot','add','Visible','off','XTic
     plot([1430 8 8 1430 1430], [10 10 157 157 10], 'k-', 'LineWidth', 6)
     
 % hBx = axes('Parent',hFig,'Units','pixels','NextPlot','add','Visible','off','XTick',[],'YTick',[],'Position',[20 50 1930 160]);
-hBx = axes('Parent',hFig,'Units','pixels','NextPlot','add','Visible','off','XTick',[],'YTick',[],'Position',[20 50 2010 160]);
+hBx = axes('Parent',hFig,'Units','pixels','NextPlot','add','Visible','off','XTick',[],'YTick',[],'Position',[20 50 2020 160]);
 
     specgram(w(11).duet(tt), 512, w(11).Fs, [], round(0.95*512));
     ug = flipud(gray); colormap(ug); caxis ([-20 33]);
