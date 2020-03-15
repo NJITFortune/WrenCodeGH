d2e3d7f7c686ba048c42502e915c039dbb0a6de9
Eric Fortune <eric.fortune@njit.edu>
2020-03-15 14:14:08 -0400

    / ‘WrenLab/w1022MovieMaker.m’

--- WrenLab/w1022MovieMaker.m
+++ WrenLab/w1022MovieMaker.m
@@ -79,7 +79,7 @@ fill([specpos+w(12).syl(f).tim(1), maxX, maxX, specpos+w(12).syl(f).tim(1)], ...
            femalespkidx = find(w(12).Cspikes{j} < curtim - specpos & w(12).Cspikes{j} > -specpos);
            
            for k = 1:length(malespkidx)
-               plot([specpos+w(11).Cspikes{j}(malespkidx(k)), specpos+w(11).Cspikes{j}(malespkidx(k))], [100+(j*200), 200+(j*200)+180], 'b-', 'LineWidth', 1);
+               plot([specpos+w(11).Cspikes{j}(malespkidx(k)), specpos+w(11).Cspikes{j}(malespkidx(k))], [100+(j*200), 100+(j*200)+180], 'b-', 'LineWidth', 1);
            end
            for k = 1:length(femalespkidx)
                plot([specpos+w(12).Cspikes{j}(femalespkidx(k)), specpos+w(12).Cspikes{j}(femalespkidx(k))], [3900+(j*200), 3900+(j*200)+180], 'm-', 'LineWidth', 1);
