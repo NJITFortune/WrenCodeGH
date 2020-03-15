42b40dad3a9483bc16473a42f030606019967ef1
Eric Fortune <eric.fortune@njit.edu>
2020-03-14 21:54:41 -0400

    / ‘WrenLab/w1022MovieMaker.m’

--- WrenLab/w1022MovieMaker.m
+++ WrenLab/w1022MovieMaker.m
@@ -30,10 +30,10 @@ hAx = axes('Parent',hFig,'Units','pixels','NextPlot','add','Visible','off','XTic
 im = readFrame(v);
 image(flipud(im));
 hold on;
-plot([10 1430], [150 150], 'k-', 'LineWidth', 6)
+plot([10 1430], [155 155], 'k-', 'LineWidth', 6)
 plot([10 1430], [10 10], 'k-', 'LineWidth', 6)
-plot([10 10], [10 150], 'k-', 'LineWidth', 6)
-plot([1430 1430], [10 150], 'k-', 'LineWidth', 6)
+plot([10 10], [10 155], 'k-', 'LineWidth', 6)
+plot([1430 1430], [10 155], 'k-', 'LineWidth', 6)
 
 hBx = axes('Parent',hFig,'Units','pixels','NextPlot','add','Visible','off','XTick',[],'YTick',[],'Position',[20 50 1930 160]);
 specgram(w(11).duet, 512, w(11).Fs, [], round(0.95*512));
