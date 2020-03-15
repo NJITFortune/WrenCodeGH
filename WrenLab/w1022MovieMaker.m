6a38f70fd5da6cbee8947f07e32d414079f0634d
Eric Fortune <eric.fortune@njit.edu>
2020-03-15 11:12:12 -0400

    / ‘WrenLab/w1022MovieMaker.m’

--- WrenLab/w1022MovieMaker.m
+++ WrenLab/w1022MovieMaker.m
@@ -6,7 +6,7 @@ load /Users/eric/Sync/Wren/ChronicCompleat2019f.mat
 %     Avideo = Svideo(:,1); % Use only one channel of the audio
 
 rango = [-3.0248, 9.9904]; % Range from the w(11).tim that matches the video file
-specpos = rango(1);
+specpos = -rango(1);
 
 figure(1); clf; hold on;
 
