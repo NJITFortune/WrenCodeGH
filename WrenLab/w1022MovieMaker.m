217ab74476493d4915d030ab6b29579a05a3b79e
Eric Fortune <eric.fortune@njit.edu>
2020-03-15 14:16:01 -0400

    / ‘WrenLab/w1022MovieMaker.m’

--- WrenLab/w1022MovieMaker.m
+++ WrenLab/w1022MovieMaker.m
@@ -58,12 +58,12 @@ hBx = axes('Parent',hFig,'Units','pixels','NextPlot','add','Visible','off','XTic
         
         if w(11).syl(f).tim(1) < curtim - specpos
            if w(11).sylsex(f) == 1 % Male
-           maxX = min([w(idx(11)).syl(f).tim(2)+specpos, curtim]);    
+           maxX = min([w(11).syl(f).tim(2)+specpos, curtim]);    
 fill([specpos+w(11).syl(f).tim(1), maxX, maxX, specpos+w(11).syl(f).tim(1)], ...
     [750, 750, 4000, 4000], 'c', 'FaceAlpha', 0.1, 'LineStyle', 'none');
            end
            if w(11).sylsex(f) == 2 % Female
-           maxX = min([w(idx(12)).syl(f).tim(2)+specpos, curtim]);    
+           maxX = min([w(12).syl(f).tim(2)+specpos, curtim]);    
 fill([specpos+w(12).syl(f).tim(1), maxX, maxX, specpos+w(12).syl(f).tim(1)], ...
     [750, 750, 4000, 4000], 'm', 'FaceAlpha', 0.1, 'LineStyle', 'none');
            end
