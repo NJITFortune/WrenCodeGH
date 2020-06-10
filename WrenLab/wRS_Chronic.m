9afdb7a36beb31c26bb4c7dd7ce33161396ba21f
Eric Fortune <eric.fortune@gmail.com>
2020-06-10 16:41:42 -0400

    / ‘WrenLab/wRS_Chronic.m’

--- WrenLab/wRS_Chronic.m
+++ WrenLab/wRS_Chronic.m
@@ -48,7 +48,7 @@ sumdat.mDuetAuto.SPS = []; sumdat.fDuetAuto.SPS = [];
 
 %% Loop to calculate RS values for each pair of wrens   
     
-for curpair = 2:6 % 1:length(Cspon)
+for curpair = 2:6 % 1:length(Cspon)  %%%% BUMMER %%%%
     
     % Solo syllables MALE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     if ~isempty(msolosyls{curpair}) % Male sang solo syllables
