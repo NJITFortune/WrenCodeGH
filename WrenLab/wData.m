77bd2ff40818a6f553e4b96d018f8d82c0ca63bb
Eric Fortune <eric.fortune@gmail.com>
2020-07-13 11:19:57 -0400

    / ‘WrenLab/wData.m’

--- WrenLab/wData.m
+++ WrenLab/wData.m
@@ -1,13 +1,15 @@
-function [msolosyls, mduetsyls, fsolosyls, fduetsyls, Cspon, Aspon] = wData
+function [msolosyls, mduetsyls, fsolosyls, fduetsyls, Cspon, Aspon, birdlist] = wData
 % Usage [msolosyls, mduetsyls, fsolosyls, fduetsyls, Cspon, Aspon] = wData
 % This function only returns the identities of syllables for use in the
 % analysis of the wren chronic data.  Data ChronicCompleat2018d.mat
 
 %% List of groups of birds
 
-
-
-
+birdlist{1} = 16:-1:1; % All compleat data with both ACUTE (urethane) AND CHRONIC (awake)
+birdlist{2} = [3 4 5 6 7 8 9 10 11 12 15 16];  % Only DUETS with Female Solo Syllables
+birdlist{3} = [3 4 5 6 7 8 9 10 11 12 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42]; % Everything with Female Solo Syllables
+birdlist{4} = [1 2 3 4 5 6 13 14 17 18]; % Only DUETS with Male Solo Syllables
+birdlist{5} = length(in):-1:1;
 
 %% List of Chronic singing data with syllable indices and locations for spontaneous activity
 
