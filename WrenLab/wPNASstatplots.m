26dd0c4d702f95c3a61c8c5dc4b289a71313cce0
Eric Fortune <eric.fortune@gmail.com>
2020-07-28 18:53:25 -0400

    / ‘WrenLab/wPNASstatplots.m’

--- WrenLab/wPNASstatplots.m
+++ WrenLab/wPNASstatplots.m
@@ -48,7 +48,8 @@ sumdat.mDuetAuto.SPS = []; sumdat.fDuetAuto.SPS = [];
 
 %% Loop to calculate RS values for each pair of wrens   
     
-for curpair = [1 2 3 7 9
+for curpair = [1 2 3 7 9] % The male matched pairs
+% for curpair = [1 2 3 7 9] % The male matched pairs
     
     %birdlist{4}   % 2 is matched female data (only duets that also had female solo syllables)
                             % 4 is matched male data (only duets that also had male solo syllables)
