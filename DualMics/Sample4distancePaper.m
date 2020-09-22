479a8569834673819fbb1ff64110d69fecd3f36e
Eric Fortune <eric.fortune@gmail.com>
2020-09-22 15:45:14 -0400

    / ‘DualMics/Sample4distancePaper.m’

--- DualMics/Sample4distancePaper.m
+++ DualMics/Sample4distancePaper.m
@@ -1,12 +1,12 @@
 % load('/Users/eric/Sync/Wren/DistanceData-V10.mat')
 
-
+[
 figure(1); clf
 subplot(211); 
     specgram(dd(12).femMic, 1024, dd(12).Fs, [], 1000);
     yarg = flipud(gray);
     colormap(yarg);
-    ylim([0 5000]);
+    ylim([500 4500]);
     xlim([0.6 5.6]);
     caxis([-30 35]);
     
