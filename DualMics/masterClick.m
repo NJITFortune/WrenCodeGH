705ff1a2f68f7b920df792542d22287fd1f38780
Eric Fortune <eric.fortune@gmail.com>
2021-01-18 16:32:56 -0500

    / ‘DualMics/masterClick.m’

--- DualMics/masterClick.m
+++ DualMics/masterClick.m
@@ -159,9 +159,9 @@ end
     
 %% Assign sex
 
-maleMicAmp = 0; femMicAmp = 0;
-
     for pp = 1:length(msyls)
+
+        maleMicAmp = 0; femMicAmp = 0;
      
         for jj = 1:length(msyls(pp).num)
           maleMicAmp = maleMicAmp + median(abs(out.maleMic(out.msyl(msyls(pp).num(jj)).sylidx(1):out.msyl(msyls(pp).num(jj)).sylidx(2))));
