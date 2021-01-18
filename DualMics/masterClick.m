3494227a6a1148c42ddac11debe1411b24c7c79a
Eric Fortune <eric.fortune@gmail.com>
2021-01-18 16:13:26 -0500

    / ‘DualMics/masterClick.m’

--- DualMics/masterClick.m
+++ DualMics/masterClick.m
@@ -164,8 +164,8 @@ maleMicAmp = 0; femMicAmp = 0;
     for pp = 1:length(msyls)
      
         for jj = 1:length(msyls(pp).num)
-          maleMicAmp = maleMicAmp + mean(abs(out.maleMic(out.msyl(msyls(pp).num(jj)).sylidx(1):out.msyl(msyls(pp).num(jj)).sylidx(2))));
-          femMicAmp = femMicAmp +   mean(abs(out.femMic(out.fsyl(fsyls(pp).num(jj)).sylidx(1):out.fsyl(fsyls(pp).num(jj)).sylidx(2))));
+          maleMicAmp = maleMicAmp + median(abs(out.maleMic(out.msyl(msyls(pp).num(jj)).sylidx(1):out.msyl(msyls(pp).num(jj)).sylidx(2))));
+          femMicAmp = femMicAmp +   median(abs(out.femMic(out.fsyl(fsyls(pp).num(jj)).sylidx(1):out.fsyl(fsyls(pp).num(jj)).sylidx(2))));
         end
       
         if maleMicAmp > femMicAmp % This is a male syllable
