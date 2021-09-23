1a3500361564f462cf5011c6b153438ee35da751
Eric Fortune <eric.fortune@gmail.com>
2021-09-23 15:22:00 -0400

    / ‘DualMics/ClickyClicky.m’

--- DualMics/ClickyClicky.m
+++ DualMics/ClickyClicky.m
@@ -221,8 +221,10 @@ for jk = length(fsyls):-1:1
     currFemaleSequence(fsyls(jk).num) = jk;
 end
 
+scr_siz = get(0,'ScreenSize') ;
 figure(3); clf; 
-set(gcf, 'Position', [50 50 scr_siz(3)-200 scr_siz(4)-200])
+    set(gcf, 'Position', [50 50 scr_siz(3)-200 scr_siz(4)-200])
+
 subplot(211); specgram(out.maleMic, 1024, out.Fs); ylim([200 5200]);
 caxis([-10 40]); colormap(flipud(gray));
 hold on;
