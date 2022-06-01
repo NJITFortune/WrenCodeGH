0bc6a51571fa4ab38c909a55a98efd4928077878
Eric Fortune <eric.fortune@gmail.com>
2022-06-01 16:59:32 -0400

    + ‘General/FrontiersSpec2022.m’

---
+++ General/FrontiersSpec2022.m
@@ -0,0 +1,8 @@
+% Spectrogram figure for Frontiers Review 2022
+
+[s, Fs] = audioread('/Users/eric/SparkleShareOLD/WrenMS/Science2011/PlainTail_DuetSwitch.wav');
+
+tt = [34.9 42.82];
+figure(1); specgram(s(tim > tt(1) & tim < tt(2)), 2048, Fs, [], 2000);
+figure(1); colormap(flipud(gray)); caxis([-10 30]); ylim([500 4200])
+
