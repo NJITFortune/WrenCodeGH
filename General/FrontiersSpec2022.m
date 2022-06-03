0b80b248a732c34c9cdc84b43516d58cff3adb1b
Eric Fortune <eric.fortune@gmail.com>
2022-06-03 16:42:01 -0400

    / ‘General/FrontiersSpec2022.m’

--- General/FrontiersSpec2022.m
+++ General/FrontiersSpec2022.m
@@ -2,8 +2,17 @@
 
 [s, Fs] = audioread('/Users/eric/SparkleShareOLD/WrenMS/Science2011/PlainTail_DuetSwitch.wav');
 
+%%
 tt = [34.9 42.82];
 figure(1); specgram(s(tim > tt(1) & tim < tt(2)), 2048, Fs, [], 2000);
 figure(1); colormap(flipud(gray)); caxis([-10 30]); ylim([500 4200])
 set(gcf, 'Renderer', 'painters');
+
+%% 
+
+load /Users/eric/Sync/Wren/ChronicCompleat2021CodesG.mat
+
+%%
+
+
 
