43679500f4e13e24f7b626a3bd0dcc10b8e3bd66
Eric Fortune <40925700+NJITFortune@users.noreply.github.com>
2025-02-24 00:34:10 -0500

    Added Rolling Specgram code

---
+++ WrenLab/wMakeRollingSpectrogramVideo.m
@@ -0,0 +1,168 @@
+% Make the video that plays spikes along with a rolling line on 
+% a plot of the spectrogram for our best case, M17 (indices 1 and 2)
+% You can edit idx below to any pair (i.e. idx = [odd, even];)
+% and you can adust the time window of the video (rango) in seconds
+% from the start of the duet.
+
+%% Make the audio file
+idx = [1, 2] ; % This is the Male (odd)
+rango = [0.1, 5.8];
+
+    Fs = w(idx(1)).Fs;
+    
+% Time is from the start of the duet, which means there are
+% negative times possible (i.e. solo syllables before the duet)
+% specgram has only positive values from the start of the signal
+% that you give it. This code snipette solves this stupid problem
+% using a stupid solution.
+
+    specpos = 0; % Position of the moving line that indicates current time on the specgram
+    if rango(1) < 0 
+        specpos = abs(rango(1)); 
+    end
+    if rango(1) > 0 
+        specpos = -rango(1); 
+    end
+
+
+%% Make the Fake Spikes
+
+    spiketim = 1/Fs:1/Fs:0.005; % 4 msec duration for our fake spikes
+    len = length(spiketim);
+    rmp = 1/floor(len/2):1/floor(len/2):1;
+
+ % We had 4 channels of neurophysiological data for the female and for the male
+ % Make blips for each spike on each channel.
+    for kk = 1:4
+        fspike(:,kk) = sin(2*pi*(1200+(50*kk))*spiketim) * 0.5 ; % 800 Hz for females
+            fspike(1:length(rmp),kk) = fspike(1:length(rmp),kk)' .* rmp;
+            fspike(end+1-length(rmp):end,kk) = fspike(end+1-length(rmp):end,kk)' .* rmp(end:-1:1);
+            
+        mspike(:,kk) = sin(2*pi*(800+(80*kk))*spiketim) * 0.25 ; % 300 Hz for males
+            mspike(1:length(rmp),kk) = mspike(1:length(rmp),kk)' .* rmp;
+            mspike(end+1-length(rmp):end,kk) = mspike(end+1-length(rmp):end,kk)' .* rmp(end:-1:1);
+    end
+    
+    outim = w(idx(1)).tim(w(idx(1)).tim > rango(1) & w(idx(1)).tim < rango(2));
+    outduet = w(idx(1)).duet(w(idx(1)).tim > rango(1) & w(idx(1)).tim < rango(2));
+    
+% Make the spike sound trains
+    fem = zeros(1,length(outim));
+    mal = zeros(1,length(outim));
+
+% Male (odd)
+
+for k = 1:4
+    spkidx = find(w(idx(1)).Cspikes{k} > rango(1) & w(idx(1)).Cspikes{k} < rango(2));
+    for j = 1:length(spkidx)   
+        curidx = find(outim >= w(idx(1)).Cspikes{k}(spkidx(j)), 1, 'first');    
+        if curidx+len-1 < length(outim)  % Need the if not to go over the end.   
+            mal(curidx:curidx+len-1) = mal(curidx:curidx+len-1) + mspike(:,k)';
+            % mal(curidx:curidx+len-1) = mspike;
+        end
+    end
+end
+
+% Female (even)
+
+for k = 1:4
+    spkidx = find(w(idx(2)).Cspikes{k} > rango(1) & w(idx(2)).Cspikes{k} < rango(2));
+    for j = 1:length(spkidx)   
+        curidx = find(outim >= w(idx(2)).Cspikes{k}(spkidx(j)), 1, 'first');    
+        if curidx+len-1 < length(outim)  % Need the if not to go over the end.   
+            %fem(curidx:curidx+len-1) = fspike;
+            fem(curidx:curidx+len-1) = fem(curidx:curidx+len-1) + fspike(:,k)';
+        end
+    end
+end
+
+%% Save spike channels audio files alone and together
+
+spks = fem' + mal';
+
+combo = spks + (outduet*0.8);
+femonly = (outduet*0.8) + fem';
+malonly = (outduet*0.8) + mal';
+    audiowrite('combo.wav', combo, 10000);
+    audiowrite('malonly.wav', malonly, 10000);
+    audiowrite('femonly.wav', femonly, 10000);
+
+%% Quick plot in prep for the video
+
+    vFs = 30; % frames per second
+
+figure(1); clf; 
+    set(gcf,'position',[100,100,640,480]);
+    specgram(outduet, 512, Fs);
+    colormap('HOT');
+    % truesize([960 1280]); 
+    % truesize([480 640]);
+    % specgram(outduet, 512, Fs);
+    caxis([-25 25])
+    cmp = flipud(gray);
+    %colormap(cmp);
+    hold on;
+
+    %% Generate the video
+    
+% Initialize the "object" that will be the final movie
+    writerObj = VideoWriter('mymovie2.avi', 'Uncompressed AVI');
+    writerObj.FrameRate = 30;
+%writerObj.VideoFormat = 'RGB24';
+    %writerObj.Quality = 100;
+    open(writerObj);
+
+for vtim = outim(1):1/30:outim(end)
+    clf; 
+    specgram(outduet, 512, Fs, [], round(512*0.95));
+    colormap(cmp);
+    caxis([-25 25])
+    hold on;
+    
+    plot([specpos+vtim,specpos+vtim], [0, 5000], 'r-', 'LineWidth', 5); % Red progress line
+    
+    % Plot transparent color boxes
+    
+    for f = 1:length(w(idx(1)).syl)
+%    for f = 1:13
+        
+        if w(idx(1)).syl(f).tim(1) >= rango(1) && w(idx(1)).syl(f).tim(1) < vtim
+           maxX = min([w(idx(1)).syl(f).tim(2), vtim]);
+           if w(idx(1)).sylsex(f) == 1 % Male
+           fill([specpos+w(idx(1)).syl(f).tim(1), specpos+maxX, specpos+maxX, specpos+w(idx(1)).syl(f).tim(1)],...
+               [750, 750, 4000, 4000], 'c', 'FaceAlpha', 0.2, 'LineStyle', 'none');
+           end
+           if w(idx(1)).sylsex(f) == 2 % Female
+           fill([specpos+w(idx(1)).syl(f).tim(1), specpos+maxX, specpos+maxX, specpos+w(idx(1)).syl(f).tim(1)],...
+               [750, 750, 4000, 4000], 'm', 'FaceAlpha', 0.1, 'LineStyle', 'none');
+           end
+        
+        end
+    end
+    
+    % Plot spikes
+    
+        for j = 1:4
+           malespkidx = find(w(idx(1)).Cspikes{j} > rango(1) & w(idx(1)).Cspikes{j} < vtim);
+           femalespkidx = find(w(idx(2)).Cspikes{j} > rango(1) & w(idx(2)).Cspikes{j} < vtim);
+           
+           for k = 1:length(malespkidx)
+               plot([specpos+w(idx(1)).Cspikes{j}(malespkidx(k)), specpos+w(idx(1)).Cspikes{j}(malespkidx(k))], [200+(j*100), 200+(j*100)+90], 'b-', 'LineWidth', 1);
+           end
+           for k = 1:length(femalespkidx)
+               plot([specpos+w(idx(2)).Cspikes{j}(femalespkidx(k)), specpos+w(idx(2)).Cspikes{j}(femalespkidx(k))], [4000+(j*100), 4000+(j*100)+90], 'm-', 'LineWidth', 1);
+           end
+        end
+        
+    pause(0.1);
+    frame = getframe(gcf);
+    writeVideo(writerObj, frame);
+end
+
+
+ close(writerObj);
+ 
+ % ffmpeg -i mymovie.avi -i combo.wav -c:v copy output.avi
+ % ffmpeg -f concat -i mylist.txt -c copy combination.mp4
+ % ffmpeg -i combination.mp4 -filter:v "setpts=2.0*PTS" -filter:a "atempo=0.5" slow.mp4
+ % ffmpeg -i output2.avi -strict -2 -pix_fmt yuv420p m17spikes.mp4
